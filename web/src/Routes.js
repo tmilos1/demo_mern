29bbe44eb7c71dc86d98b4994af5cfbdbeba2d56
Milos Tijanic <tmilos1@gmail.com>
2020-06-25 20:18:27 +0200

    Removed about page

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -18,7 +18,6 @@ const Routes = () => {
       <Route path="/posts/{id:Int}/edit" page={EditPostPage} name="editPost" />
       <Route path="/posts/{id:Int}" page={PostPage} name="post" />
       <Route path="/posts" page={PostsPage} name="posts" />
-      <Route path="/about" page={AboutPage} name="about" />
       <Route path="/" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
     </Router>
--- web/src/pages/AboutPage/AboutPage.js
+++
@@ -1,9 +0,0 @@
-import BlogLayout from 'src/layouts/BlogLayout'
-
-const AboutPage = () => {
-  return (
-    <BlogLayout>This is About Page</BlogLayout>
-  )
-}
-
-export default AboutPage
